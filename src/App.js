d5e8a45fd30f99ce197fdbd2f439f2e5c3a16de0
Marek Klatt <531devv@gmail.com>
2020-02-04 20:24:02 +0100

    Adding footer component and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import QuizzContainer from './Quizz/QuizzContainer';
+import Footer from './Footer/Footer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <QuizzContainer />
+      <div className="App">
+        <QuizzContainer />
+        <Footer />
+      </div>
     );
   }
 }
