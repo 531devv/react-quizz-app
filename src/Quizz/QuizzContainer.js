345bb793e73b7cc117785fd5579ba382addb1e6c
Marek Klatt <531devv@gmail.com>
2020-02-04 19:30:55 +0100

    Adding styles to QuizzContainer component

--- src/Quizz/QuizzContainer.js
+++ src/Quizz/QuizzContainer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import QuizzQuestions from './QuizzQuestions';
 import QuizzAnswers from './QuizzAnswers';
+import './css/QuizzContainer.css';
 
 export default class QuizzContainer extends React.Component {
     render() {
         return(
-            <div>
+            <div className="quizz-container">
                 <QuizzQuestions />
                 <QuizzAnswers />
             </div>
