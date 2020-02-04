c92c653c3305fad29bdc7abf99ee25c26b3dd0b5
Marek Klatt <531devv@gmail.com>
2020-02-04 19:24:32 +0100

    Adding quizz components structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import QuizzContainer from './QuizzContainer';
+import QuizzContainer from './Quizz/QuizzContainer';
 import './App.css';
 
 class App extends Component {
