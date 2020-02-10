2096d31d18cbe96c198762df276eba8ef30cd67c
Marek Klatt <531devv@gmail.com>
2020-02-10 22:37:43 +0100

    Updates

---
+++ precache-manifest.accf1a0b750bf1e71572dec641479bd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533f4cd2e51108be3c61e76aad58bade",
+    "url": "/react-quizz-app/index.html"
+  },
+  {
+    "revision": "654ee35747dd0109c852",
+    "url": "/react-quizz-app/static/css/main.27e1d90b.chunk.css"
+  },
+  {
+    "revision": "8c4f82ab762f3b8a2a2a",
+    "url": "/react-quizz-app/static/js/2.0939ac2a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-quizz-app/static/js/2.0939ac2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "654ee35747dd0109c852",
+    "url": "/react-quizz-app/static/js/main.e715fd93.chunk.js"
+  },
+  {
+    "revision": "578531adbf9977d06dfd",
+    "url": "/react-quizz-app/static/js/runtime-main.d97fabe0.js"
+  }
+]);
